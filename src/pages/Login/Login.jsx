8638014b093f1514894639faa68e8383a067fe20
DariaPonoran <dariaponoran@gmail.com>
2023-05-18 16:07:44 +0300

    Login works

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,8 +2,42 @@ import axios from 'axios'
 import React from 'react'
 import { useEffect } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import { useState } from 'react'
 import './Login.css'
 function Login({ onLogin }) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [message, setMessage] = useState('')
+  const handleSubmit = (event) => {
+    console.log('handleSubmit ran')
+    event.preventDefault()
+    console.log('email', email)
+    console.log('password', password)
+    getUser(email, password)
+    setEmail('')
+    setPassword('')
+  }
+  async function getUser(email, password) {
+    try {
+      const response = await axios.get(
+        'http://localhost:8080/login/emailAndPassword',
+        {
+          params: {
+            email: email,
+            password: password,
+          },
+        }
+      )
+      if (!!response.data.length) {
+        console.log('login')
+      } else {
+        console.log('wrong')
+      }
+      console.log('response', response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
   const fetchUsers = async () => {
     try {
       const result = await axios.get('http://localhost:8080/users')
@@ -11,24 +45,44 @@ function Login({ onLogin }) {
     } catch (error) {}
   }
 
-  useEffect(() => {
-    fetchUsers()
-  }, [])
+  //useEffect(() => {
+  // fetchUsers()
+  // }, [])
+  // useEffect(() => {
+  //   getUser()
+  // }, [])
 
   return (
     <div class='container-login'>
       <div class='form'>
         <form>
           <p>Welcome</p>
-          <input type='email' placeholder='Email' />
+          <input
+            id='email'
+            name='email'
+            type='text'
+            placeholder='Email'
+            onChange={(event) => setEmail(event.target.value)}
+            value={email}
+          />
           <br />
-          <input type='password' placeholder='Password' />
+          <input
+            id='password'
+            name='password'
+            type='password'
+            placeholder='Password'
+            onChange={(event) => setPassword(event.target.value)}
+            value={password}
+          />
           <br />
-          <button className='btn'>Login</button>
+          <button className='btn' onClick={handleSubmit}>
+            Login
+          </button>
           <br />
           <button className='btn'>Forgot Password?</button>
           <br />
           <button className='btn'>Don't have an account? Sign Up</button>
+          <h2>{message}</h2>
         </form>
       </div>
       <div class='drops'>
